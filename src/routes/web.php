8c1126d6b62cf5e7dedfdd40d6119ada43b681c5
AbdUl Sllam Eskef <AbdUlSllam@Minarets.Local>
2019-12-14 09:59:18 +0200

    first eddtion

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,9 +4,6 @@ Route::group(['namespace' => 'GMTCC\Search\Http\Controllers'], function (){
     Route::get('/live_search/action','liveSearchController@action')->name('liveSearchController.action');    
 });
 
-Route::get('search',function(){
- return view('search_modal::search');
-});
 
 
     
